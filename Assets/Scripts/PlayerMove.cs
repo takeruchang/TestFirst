8ccd7671a73260207dbd0a451bf6ee4dc1ec8b4e
takeruchang <haitei8nerd@gmail.com>
2024-08-07 17:27:02 +0900

    2024.0807_2
    左右移動、待機のアニメーション実装。
    左右に物理演算ベースの横移動を実装

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,49 @@
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //インスペクター上で数値を設定させる
+    public float speed;
+    
+    //プライベートの変数
+    private Animator anim = null;
+    private Rigidbody2D rb = null;  
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //コンポーネントのインスタンスを指定
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キー入力されたら行動
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+
+        if(horizontalKey > 0)
+        {
+            transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            anim.SetBool("runR", true);
+            xSpeed = speed;
+        }
+        else if(horizontalKey < 0)
+        {
+            transform.localScale = new Vector3(-0.2f, 0.2f, 0.2f);
+            anim.SetBool("runR", true);
+            xSpeed = -speed;
+        }
+        else
+        {
+            anim.SetBool("runR", false);
+            xSpeed = 0.0f;
+        }
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
+    }
+}
